5dd0ef5a030d747728512a8714dc40a7a387a913
huyen <dothithanhhuyen2297@gmail.com>
2019-03-03 14:19:44 +0700

    H_Module con

---
+++ ChildrenCore/src/main/java/tdt/tranning/maven/mathutils/MathUtils.java
@@ -0,0 +1,17 @@
+package tdt.tranning.maven.mathutils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MathUtils {
+	public static void main(String[] args) {
+		String text1 = "0123a4";
+		String text2 = "01234";
+
+		boolean result1 = StringUtils.isNumeric(text1);
+		boolean result2 = StringUtils.isNumeric(text2);
+
+		System.out.println(text1 + " is a numeric ? " + "\n" + result1);
+		System.out.println(text2 + " is a numeric ? " + "\n" + result2);
+
+	}
+}
